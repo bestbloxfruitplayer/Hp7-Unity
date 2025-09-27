da5b33360a474913014a0c8dc987fed92b567bfc
bestbloxfruitplayer <darkminh2012@gmail.com>
2025-09-27 16:48:55 +0700

    init unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpforce = 15f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private Animator animator;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb= GetComponent<Rigidbody2D>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+        UpdateAnimation();
+    }
+    private void HandleMovement()
+    {
+        float moveinput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveinput * moveSpeed, rb.linearVelocity.y);
+        if (moveinput > 0) transform.localScale = new Vector3(1, 1, 1);
+        else if(moveinput<0)transform.localScale=new Vector3(-1, 1, 1);
+
+    }
+    private void HandleJump()
+    {
+        if(Input.GetButtonDown("Jump")&&isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+        }
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void UpdateAnimation()
+    {
+        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
+        bool isJumping = !isGrounded;
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isJumping", isJumping);
+    }
+}
+
+        
